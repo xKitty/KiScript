a13495fe4dc7f70df0e982cbc4f64f05aa33aae2
xKitty <kitty@xdk2.net>
2011-12-20 18:07:33 -0500

    Added more CTCP commands, basic CTCP handler

--- KiScript.pl
+++ KiScript.pl
@@ -21,26 +21,113 @@ package IRC::XChat::KiScript;
 use strict;
 use Xchat qw( :all );
 my $PKG = __PACKAGE__;
+my $XCHATVER = IRC::get_info(0);
 my $VERSION = "0.0";
-my $REVISION = "0002";
+my $REVISION = "0003";
 
-IRC::register("KiScript", "r$REVISION", "", "");
+Xchat::register("KiScript", "r$REVISION", "Kitty's XChat Script", "${PKG}::unload");
 
-IRC::add_command_handler("TIME", "${PKG}::ctcptime");
+IRC::add_message_handler("PRIVMSG", "${PKG}::privmsghandler");
+IRC::add_message_handler("NOTICE", "${PKG}::noticehandler");
+
+IRC::add_command_handler("AVATAR", "${PKG}::ctcpavatar");
+IRC::add_command_handler("CLIENTINFO", "${PKG}::ctcpclientinfo");
 IRC::add_command_handler("FINGER", "${PKG}::ctcpfinger");
+IRC::add_command_handler("PAGE", "${PKG}::ctcppage");
+##ping needs to have a proper notice handler, done in script
+##xchat does not recognize our ping sends, will be fixed in next rev
+#IRC::add_command_handler("PING", "${PKG}::ctcpping");
+IRC::add_command_handler("SOURCE", "${PKG}::ctcpsource");
+IRC::add_command_handler("TIME", "${PKG}::ctcptime");
+IRC::add_command_handler("USERINFO", "${PKG}::ctcpuserinfo");
+IRC::add_command_handler("VERSION", "${PKG}::ctcpversion");
 
-sub ctcptime {
+sub ctcpavatar {
+	IRC::print("Nope, Chuck Testa!  (Not implemented yet.)");
+}
+
+sub ctcpclientinfo {
 	my $target = @_[0];
-	IRC::print("Sending CTCP TIME to $target");
-	IRC::send_raw("PRIVMSG $target :TIME");
-	return(1);
+	IRC::print("Sending CTCP CLIENTINFO to $target");
+	IRC::send_raw("PRIVMSG $target :CLIENTINFO");
+	return Xchat::EAT_ALL;
 }
 
 sub ctcpfinger {
 	my $target = @_[0];
 	IRC::print("Sending CTCP FINGER to $target");
 	IRC::send_raw("PRIVMSG $target :FINGER");
-	return(1);
+	return Xchat::EAT_ALL;
+}
+
+sub ctcpping {
+##Needs to save the timestamp and wait for a reply
+	my $target = @_[0];
+	my @time = localtime(time);
+	my $timestamp = "@time[5]@time[4]@time[3]@time[2]@time[1]@time[0]";
+	IRC::print("Sending CTCP PING to $target");
+	IRC::send_raw("PRIVMSG $target :PING $timestamp");
+	return Xchat::EAT_ALL;
+}
+
+sub ctcppage {
+	##Needs to send an actual page
+	my $target = @_[0];
+	IRC::print("Sending CTCP PAGE to $target");
+	IRC::send_raw("PRIVMSG $target :PAGE");
+	return Xchat::EAT_ALL;
+}
+
+sub ctcpsource {
+	my $target = @_[0];
+	IRC::print("Sending CTCP SOURCE to $target");
+	IRC::send_raw("PRIVMSG $target :SOURCE");
+	return Xchat::EAT_ALL;
+}
+
+sub ctcptime {
+	my $target = @_[0];
+	IRC::print("Sending CTCP TIME to $target");
+	IRC::send_raw("PRIVMSG $target :TIME");
+	return Xchat::EAT_ALL;
+}
+
+sub ctcpuserinfo {
+	my $target = @_[0];
+	IRC::print("Sending CTCP USERINFO to $target");
+	IRC::send_raw("PRIVMSG $target :USERINFO");
+	return Xchat::EAT_ALL;
+}
+
+sub ctcpversion {
+	my $target = @_[0];
+	IRC::print("Sending CTCP VERSION to $target");
+	IRC::send_raw("PRIVMSG $target :VERSION");
+	return Xchat::EAT_ALL;
+}
+
+sub privmsghandler {
+	my $raw = shift;
+	my @rawarray = split(' ', $raw);
+	if (@rawarray[3] =~ /:/) {
+		my @raw2 = split('', $raw);
+		## Source from the person, not the medium.
+		IRC::print("CTCP to	 @rawarray[2], DATA: @raw2[1]");
+		my @notgood = split(' ', @raw2[1]);
+		if (@notgood[0] =~ "PING") {
+			IRC::print("u r ping bro");
+		}
+	}
+}
+
+sub noticehandler {}
+
+sub parsetm {
+	
+}
+sub unload {
+	IRC::print("13Ki09Script by 13Kitty Unloaded");
 }
 
 IRC::print("13Ki09Script by 13Kitty Loaded (v$VERSION r$REVISION)");
+
